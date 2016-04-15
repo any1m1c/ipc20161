64d336d1bcf5850f14380ce8fe90c6ffe7ceb35a
any1m1c <anymendescarvalho@gmail.com>
2016-04-15 16:33:33 -0300

    Update ipc_lista1.7.py

--- lista1/ipc_lista1.7.py
+++ lista1/ipc_lista1.7.py
@@ -13,4 +13,4 @@ altura = input("Digite a altura do quadrado em metros: ")
 largura = input("Digite a largura do quadrado em metros: ")
 a = altura * largura
 print "A área é: m²" ,(a)
-print
+print 
