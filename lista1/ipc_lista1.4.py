4c4bce932f348747f0afad13b1161a2ec3275013
any1m1c <anymendescarvalho@gmail.com>
2016-04-15 14:26:28 -0300

    Update ipc_lista1.4.py

--- lista1/ipc_lista1.4.py
+++ lista1/ipc_lista1.4.py
@@ -7,7 +7,7 @@
 #
 #Faça um Programa que peça as 4 notas bimestrais e mostre a media
 nota1 = int(input("Digite a primeira nota do bimestre: ")
-nota2 = int(input("Digite a segunda nota do bimestre: ")
+nota2 = int(input("Digite a segunda nota do bimestre: "))
 nota3 = int(input("Digite a terceira nota do bismestre: "))
 
 
