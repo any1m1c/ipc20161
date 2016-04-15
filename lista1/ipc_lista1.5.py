11e5b09977316deefce1db3a4a91df9143173e83
any1m1c <anymendescarvalho@gmail.com>
2016-04-15 14:41:38 -0300

    Update ipc_lista1.5.py

--- lista1/ipc_lista1.5.py
+++ lista1/ipc_lista1.5.py
@@ -9,4 +9,4 @@
 #Faça um programa que converta metros para centímetros.
 
 metros = input("Digite o valor em metros que deseja converter em centímetros: ")
-centimetros = metros * 10
+centimetros = metros * 100
