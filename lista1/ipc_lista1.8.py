837389ce62e33c4ce643ac8333c0271c76d89b09
any1m1c <anymendescarvalho@gmail.com>
2016-04-15 20:59:05 -0300

    Update ipc_lista1.8.py

--- lista1/ipc_lista1.8.py
+++ lista1/ipc_lista1.8.py
@@ -6,4 +6,4 @@
 #
 #
 #Faça um programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. 
-Calcule e mostre o total do seu slário no
+#Calcule e mostre o total do seu slário no
