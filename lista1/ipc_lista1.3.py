d7e2e4d1e41e08f3772dc75f569d1efee3590030
any1m1c <anymendescarvalho@gmail.com>
2016-04-15 14:50:39 -0300

    Update ipc_lista1.3.py

--- lista1/ipc_lista1.3.py
+++ lista1/ipc_lista1.3.py
@@ -6,7 +6,7 @@
 #
 #
 
-#Faça um programa que peça dois números e imprima a soma
+#Faça um programa que peça dois números e imprima a soma.
 
 number1 = input("Digite o primeiro: ")
 number2 = input("Digite o segundo número: ")
