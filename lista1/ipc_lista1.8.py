9b1b0e2eaaca47fd38adb20d6d958b2a9c0fb749
any1m1c <anymendescarvalho@gmail.com>
2016-04-15 20:59:35 -0300

    Update ipc_lista1.8.py

--- lista1/ipc_lista1.8.py
+++ lista1/ipc_lista1.8.py
@@ -6,4 +6,4 @@
 #
 #
 #Faça um programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. 
-#Calcule e mostre o total do seu salário no
+#Calcule e mostre o total do seu salário no mês
