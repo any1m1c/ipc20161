fa5612b779753fd2704b968b4f41d2123718d4d8
any1m1c <anymendescarvalho@gmail.com>
2016-04-16 10:02:42 -0300

    Update ipc_lista1.12.py

--- lista1/ipc_lista1.12.py
+++ lista1/ipc_lista1.12.py
@@ -13,4 +13,4 @@ altura = input("Digite sua altura em metros, separa por ponto (ex: 1.70): ")
 vT = 72.7 * altura
 resultado = vT - 58
 
-print "Seu peso ideal e"
+print "Seu peso ideal e ",resultado
