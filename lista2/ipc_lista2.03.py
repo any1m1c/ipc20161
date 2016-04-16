ff990f7edb6161b51494a04f44244510bc012680
any1m1c <anymendescarvalho@gmail.com>
2016-04-16 08:37:00 -0300

    Update ipc_lista2.03.py

--- lista2/ipc_lista2.03.py
+++ lista2/ipc_lista2.03.py
@@ -6,7 +6,7 @@
 #
 #
 
-#
+##Faça um Programa que verifique se uma letra digitada é "F" ou "M". Conforme a letra escrever: F - Feminino, M - Masculino, Sexo Inválido. 
  
 
 sexo = raw_input("Informe o sexo F para Feminino e M para Masculino: ")
