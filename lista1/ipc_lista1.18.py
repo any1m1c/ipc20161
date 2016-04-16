7ed6d1a69886b5abb8364977257c811a319d4cee
any1m1c <anymendescarvalho@gmail.com>
2016-04-16 12:20:43 -0300

    Update ipc_lista1.18.py

--- lista1/ipc_lista1.18.py
+++ lista1/ipc_lista1.18.py
@@ -13,4 +13,4 @@ velocidade = input("Informe a velocidade de sua internet (em Mbps): ")
 tempo = arquivo / velocidade
 minuto = tempo / 60.0
 print
-print " O tempo aproximado para download do arquivo
+print " O tempo aproximado para download do arquivo usando este link e de: 
