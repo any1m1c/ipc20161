64a6fc0ebc9fc4ab3e6cd753d5103403468dede4
any1m1c <anymendescarvalho@gmail.com>
2016-04-16 09:02:04 -0300

    Update ipc_lista2.05.py

--- lista2/ipc_lista2.05.py
+++ lista2/ipc_lista2.05.py
@@ -5,4 +5,4 @@
 #
 #
 #
-#Faça um programa para  a leitura de duas notas parciais de um aluno. O programa deve calcular a média alcançada
+#Faça um programa para  a leitura de duas notas parciais de um aluno. O programa deve calcular a média alcançada por aluno e apresentar
