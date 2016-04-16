7c73da740b21063052be9ccc47d318d67699afda
any1m1c <anymendescarvalho@gmail.com>
2016-04-16 11:12:31 -0300

    Update ipc_lista1.16.py

--- lista1/ipc_lista1.16.py
+++ lista1/ipc_lista1.16.py
@@ -5,4 +5,4 @@
 #
 #
 #
-#Faça um programa para uma loja de tintas. O programa deverá pedir o tamanho em metros quadrados da área a ser pintada.
+#Faça um programa para uma loja de tintas. O programa deverá pedir o tamanho em metros quadrados da área a ser pintada. Considere que a cobertura da tinta é de 1 litro
