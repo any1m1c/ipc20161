ddf439fe200bee4673327ad5db86cb2b29f4e27e
any1m1c <anymendescarvalho@gmail.com>
2016-04-16 08:31:28 -0300

    Update ipc_lista2.03.py

--- lista2/ipc_lista2.03.py
+++ lista2/ipc_lista2.03.py
@@ -12,9 +12,9 @@
 sexo = raw_input("Informe seu sexo F para Feminino e M para Masculino: ")
 
 if (sexo == "M"):
- print ("Sexo Masculino.")
+ print "Sexo Masculino."
 else: 
     if (sexo == "F"):
-         print ("Sexo feminino.")
+         print "Sexo feminino."
     else:
          print ("Sexo Invalido.")
