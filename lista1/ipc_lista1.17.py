429ad8cf4025b1d867716fb75e5ef596b892b56f
any1m1c <anymendescarvalho@gmail.com>
2016-04-16 11:29:55 -0300

    Update ipc_lista1.17.py

--- lista1/ipc_lista1.17.py
+++ lista1/ipc_lista1.17.py
@@ -10,7 +10,7 @@ import math
 
 #Calculo para verificar quantas latas/galoes de tintas sera necessarias e o valor delas
 
-metros =
+metros = input("Entre como o tamnho em metros quadrdados"
 /
 /
 /
