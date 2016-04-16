4a448e250457240008618e54a33a298cc4e44b66
any1m1c <anymendescarvalho@gmail.com>
2016-04-16 09:24:45 -0300

    Update ipc_lista2.06.py

--- lista2/ipc_lista2.06.py
+++ lista2/ipc_lista2.06.py
@@ -8,5 +8,5 @@
 #Faça um programa que leia três números e mostre o maior deles.
 
 number1 = int(input("Insira um numero: ")
-number2 = int(input("Insira outro numero: ) "
+number2 = int(input("Insira outro numero: ")
 number3 = int(input("Insira mais um numero: ")
