81476e297f791f73d7e3f11ad5a6c377863871cc
any1m1c <anymendescarvalho@gmail.com>
2016-04-16 09:30:34 -0300

    Update ipc_lista2.06.py

--- lista2/ipc_lista2.06.py
+++ lista2/ipc_lista2.06.py
@@ -12,8 +12,11 @@ num2 = int(input("Insira outro numero: ")
 num3 = int(input("Insira mais um numero: ")
 
 if num1>num2
-    if num1>num3
+    if num1>num3:
+        print
 if num2>num1
-    if num2>num3
+    if num2>num3:
+        print
 if num3>num1
-    if num3>num2
+    if num3>num2:
+        print
