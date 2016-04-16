a90b3a3eebf25d135238edcda7be374c5b0cf5b9
any1m1c <anymendescarvalho@gmail.com>
2016-04-15 21:03:37 -0300

    Update ipc_lista1.8.py

--- lista1/ipc_lista1.8.py
+++ lista1/ipc_lista1.8.py
@@ -8,4 +8,4 @@
 #Faça um programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. 
 #Calcule e mostre o total do seu salário no referido mês.
 
-QntHora = input("Entre com o valor de seu rendimento
+QntHora = input("Entre com o valor de seu rendimento por hora
