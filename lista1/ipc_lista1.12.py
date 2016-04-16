cf44023247762e28788f694e22a19cbcf0ef30c5
any1m1c <anymendescarvalho@gmail.com>
2016-04-16 09:59:05 -0300

    Update ipc_lista1.12.py

--- lista1/ipc_lista1.12.py
+++ lista1/ipc_lista1.12.py
@@ -5,4 +5,4 @@
 #
 #
 #
-#Tendo como dados de entrada a altura de uma pessoa, construa um algoritmo que calcule seu peso ideal, usando a seguinte fórmula:
+#Tendo como dados de entrada a altura de uma pessoa, construa um algoritmo que calcule seu peso ideal, usando a seguinte fórmula: \n (72.7*altura) - 58
