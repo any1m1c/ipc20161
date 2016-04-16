72de3fa5d2a4ae32d6c1a64955c0dce208f4dfcf
any1m1c <anymendescarvalho@gmail.com>
2016-04-15 21:18:43 -0300

    Update ipc_lista1.9.py

--- lista1/ipc_lista1.9.py
+++ lista1/ipc_lista1.9.py
@@ -5,4 +5,4 @@
 #
 #
 #
-#Faça um programa que peça a temperatura em graus Fahrenheit, transforme e mostre a temperatura em graus Celsius
+#Faça um programa que peça a temperatura em graus Fahrenheit, transforme e mostre a temperatura em graus Celsius.
